d4a0c0ac934a0e8e2088f4fb514766152e5ba950
ChouKi-7 <zq199291@gmail.com>
2024-11-23 20:31:04 +0900

    事前準備

---
+++ src/data_processing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+train_data = pd.read_csv("data/raw/train.csv")
+# print(train_data.head())
+
+test_data = pd.read_csv("data/raw/test.csv")
+# print(test_data.head())
+
+# print(train_data.info())
+# print(train_data.describe())
+print(train_data.isnull().sum())
