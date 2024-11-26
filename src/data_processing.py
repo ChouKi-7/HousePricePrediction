7c66f4b8a6b133ab534955abbe5cecf284cc9194
ChouKi-7 <zq199291@gmail.com>
2024-11-26 22:55:24 +0900

    pratice

--- src/data_processing.py
+++ src/data_processing.py
@@ -10,4 +10,20 @@ test_data = pd.read_csv("data/raw/test.csv")
 
 # print(train_data.info())
 # print(train_data.describe())
-print(train_data.isnull().sum())
+# print(train_data.isnull().sum())
+
+missdataList = []
+missing_data = train_data.isnull().sum()
+for col, missdata in missing_data.items():
+    if missdata > 0:
+        missdataList.append(col)
+        print(col)
+missing_radio = train_data.isnull().sum()/len(train_data)
+# print(missing_radio)
+
+columns_to_drop = missing_radio[missing_radio>0.8].index
+
+reduced_x = train_data.drop(columns=columns_to_drop, axis=1)
+print(reduced_x)
+# filtered = missing_data[missing_data > 1000]
+# print(filtered)
